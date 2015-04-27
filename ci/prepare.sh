594cf7b1ebdae5c4e9347d94b73a016c8ca87cd0
Simon Wheatley <simon@simonwheatley.co.uk>
2015-04-27 15:54:39 +0100

    Fix Apache conf location

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -22,7 +22,7 @@ echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
 # configure apache virtual hosts
-sudo cp -f build/wordpress-apache.conf /etc/apache2/sites-available/default
+sudo cp -f $TRAVIS_BUILD_DIR/ci/wordpress-apache.conf /etc/apache2/sites-available/default
 sudo sed -e "s?%WORDPRESS_SITE_DIR%?${WORDPRESS_SITE_DIR}?g" --in-place /etc/apache2/sites-available/default
 cat /etc/apache2/sites-available/default
 sudo service apache2 restart
