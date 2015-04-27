c8352185c4dacac97d962328ccd15f32a0784dfb
Simon Wheatley <simon@simonwheatley.co.uk>
2015-04-27 21:49:06 +0100

    Try running Composer once the file has copied. Try running Behat from the WP site plugin dir.

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -19,11 +19,6 @@ mysql -e 'GRANT ALL PRIVILEGES ON wordpress.* TO "wordpress"@"localhost" IDENTIF
 
 sudo apt-get install apache2 libapache2-mod-fastcgi
 
-# @TODO Allow a user to add their GitHub token, encrypted, so they can authenticate with GitHub and bypass API limits applied to Travis as a whole
-# https://getcomposer.org/doc/articles/troubleshooting.md#api-rate-limit-and-oauth-tokens
-# http://awestruct.org/auto-deploy-to-github-pages/ and scroll to "gem install travis"
-composer update --no-interaction --prefer-dist
-
 # enable php-fpm
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 sudo a2enmod rewrite actions fastcgi alias
@@ -47,4 +42,9 @@ $WP_CLI core config --dbname=wordpress --dbuser=wordpress --dbpass=password
 $WP_CLI core install --url=local.wordpress.dev --title="WordPress Testing" --admin_user=admin --admin_password=password --admin_email=testing@example.invalid
 cp -pr $TRAVIS_BUILD_DIR $WORDPRESS_SITE_DIR/wp-content/plugins/
 ls -al $WORDPRESS_SITE_DIR/wp-content/plugins/
+# @TODO Allow a user to add their GitHub token, encrypted, so they can authenticate with GitHub and bypass API limits applied to Travis as a whole
+# https://getcomposer.org/doc/articles/troubleshooting.md#api-rate-limit-and-oauth-tokens
+# http://awestruct.org/auto-deploy-to-github-pages/ and scroll to "gem install travis"
+cd ${WORDPRESS_SITE_DIR}/wp-content/plugins/${WORDPRESS_TEST_SUBJECT}/
+composer update --no-interaction --prefer-dist
 
