436eef9dc2278876c911218453af0571d46eea9c
Simon Wheatley <simon@simonwheatley.co.uk>
2015-04-27 15:43:09 +0100

    Set host. Install Apache and WordPress. Try to curl homepage.

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -8,13 +8,38 @@ set -ex
 
 echo 'date.timezone = "Europe/London"' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 
-# Set up the Apache virtualhost
-echo $TRAVIS_BUILD_DIR
-mkdir -p $TRAVIS_BUILD_DIR/wordpress-site/
-cp wordpress.conf /etc/apache2/sites-available/
-a2ensite apache-ci.conf
+sudo apt-get install apache2 libapache2-mod-fastcgi
+
+# set up WordPress site directory
+WORDPRESS_SITE_DIR=$(dirname $TRAVIS_BUILD_DIR)
+echo "Site dir $WORDPRESS_SITE_DIR"
+mkdir -p $WORDPRESS_SITE_DIR
+
+# enable php-fpm
+sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+sudo a2enmod rewrite actions fastcgi alias
+echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+
+# configure apache virtual hosts
+sudo cp -f build/wordpress-apache.conf /etc/apache2/sites-available/default
+sudo sed -e "s?%WORDPRESS_SITE_DIR%?${WORDPRESS_SITE_DIR}?g" --in-place /etc/apache2/sites-available/default
+cat /etc/apache2/sites-available/default
 sudo service apache2 restart
 
 # Set up the database
-mysql -e 'CREATE DATABASE wp_test;' -uroot
-mysql -e 'GRANT ALL PRIVILEGES ON wp_test.* TO "wp_cli_test"@"localhost" IDENTIFIED BY "password1"' -uroot
+mysql -e 'CREATE DATABASE wordpress;' -uroot
+mysql -e 'GRANT ALL PRIVILEGES ON wordpress.* TO "wordpress"@"localhost" IDENTIFIED BY "password"' -uroot
+
+# install WordPress
+cd $WORDPRESS_SITE_DIR
+./bin/wp core download --version=$WP_VERSION
+# @TODO Set WP_DEBUG and test for notices, etc
+./bin/wp core config --dbname=wordpress --dbuser=wordpress --dbpass=password
+./bin/wp core install --url=wordpress.dev --title="WordPress Testing" --admin_user=admin --admin_password=password --admin_email=testing@example.invalid
+# @TODO Install the plugin (or theme?)
+
+# Now check
+pwd
+ls -alh
+curl -s http://wordpress.dev/
