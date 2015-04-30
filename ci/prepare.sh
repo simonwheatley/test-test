3b056df0b51234dfe08a7bd235133802c32f3ecf
Simon Wheatley <simon@simonwheatley.co.uk>
2015-04-30 13:00:21 +0100

    Really actually set the environment var we're relying on

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -31,13 +31,14 @@ composer update --no-interaction --prefer-dist
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 sudo a2enmod rewrite actions fastcgi alias
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+# Get the WORDPRESS_FAKE_MAIL_DIR into PHP as an environment variable
+echo "env[WORDPRESS_FAKE_MAIL_DIR] = ${WORDPRESS_FAKE_MAIL_DIR}" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
 # configure apache virtual hosts
 # @TODO Allow HTTPS connections (need a solution which doesn't mind self-signed certs)
 sudo cp -f $TRAVIS_BUILD_DIR/ci/wordpress-apache.conf /etc/apache2/sites-available/default
 sudo sed -e "s?%WORDPRESS_SITE_DIR%?${WORDPRESS_SITE_DIR}?g" --in-place /etc/apache2/sites-available/default
-sudo sed -e "s?%WORDPRESS_FAKE_MAIL_DIR%?${WORDPRESS_FAKE_MAIL_DIR}?g" --in-place /etc/apache2/sites-available/default
 cat /etc/apache2/sites-available/default
 sudo service apache2 restart
 
