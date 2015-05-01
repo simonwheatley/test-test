2276b90c79137073575a073265d601f2a29c3157
Simon Wheatley <simon@simonwheatley.co.uk>
2015-05-01 17:04:33 +0100

    Check `getenv` again

--- phpinfo.php
+++ phpinfo.php
@@ -1,9 +1,9 @@
 <?php
 
-echo "ENV vars" . PHP_EOL;
+echo "Using getenv" . PHP_EOL;
 
-print_r( $_ENV );
+//print_r( $_ENV );
 
-phpinfo( INFO_CONFIGURATION );
+//phpinfo( INFO_CONFIGURATION );
 
 var_dump( getenv( 'WORDPRESS_FAKE_MAIL_DIR' ) );
