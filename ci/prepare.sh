d10725d72e6faa21c5aa3538fcf5d2c6f0dc6b29
Simon Wheatley <simon@simonwheatley.co.uk>
2015-05-01 14:54:33 +0100

    Get the status of all running services

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -29,6 +29,7 @@ sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.ph
 sudo a2enmod rewrite actions fastcgi alias
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+sudo service --status-all
 sudo service php5-fpm restart
 
 # configure apache virtual hosts
