2510a6ac189ea040c7d29f6fdf55f1ab75feebc6
Simon Wheatley <simon@simonwheatley.co.uk>
2015-05-01 17:44:56 +0100

    Remove a bunch of debugging

--- features/bootstrap/fake-mail.php
+++ features/bootstrap/fake-mail.php
@@ -1,15 +1,9 @@
 <?php
 
 define( 'WORDPRESS_FAKE_MAIL_DIVIDER', '%%===================%%' );
-var_dump( "Get env: " . getenv( 'WORDPRESS_FAKE_MAIL_DIR' ) );
 if ( ! defined( 'WORDPRESS_FAKE_MAIL_DIR' ) ) {
-	var_dump( "Trying to set WORDPRESS_FAKE_MAIL_DIR from ENV" );
 	define( 'WORDPRESS_FAKE_MAIL_DIR', getenv( 'WORDPRESS_FAKE_MAIL_DIR' ) );
-} else {
-	var_dump( "WORDPRESS_FAKE_MAIL_DIR is already set" );
 }
-var_dump( "GETENV value: " . getenv( 'WORDPRESS_FAKE_MAIL_DIR' ) );
-var_dump( "WORDPRESS_FAKE_MAIL_DIR is " . WORDPRESS_FAKE_MAIL_DIR );
 
 
 /**
